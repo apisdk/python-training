e09a57e078d2fa63771ae210376a6a26ef49df15
Taehoon.Yoon <nt10681@nhn.com>
2014-12-05 16:51:40 +0900

    change directory name. add example for printing variables

---
+++ learn-python/ex05_print_variable/printVariables.py
@@ -0,0 +1,19 @@
+my_name = 'Tony'
+my_age = 42
+my_tall_height = 180.2
+my_eye_color = 'red'
+my_hair_color = 'white'
+my_github_space = 'apisdk'
+my_repository_name = 'python-training'
+
+print 'Hello. my name is', my_name, "."     #Hello. my name is Tony .
+
+print "Hello. my name is %s." % my_name     #Hello. my name is Tony.
+
+print "my age is %d", 22
+print "I am got %s eyes, and %s hair." % (my_eye_color, my_hair_color)  # I am got red eyes, and white hair.
+
+print "my height is %d" % my_tall_height    # my height is 180
+print "my height is %f" % my_tall_height    # my height is 180.200000
+
+print "GitHub url https://github.com/%s/%s" %(my_github_space, my_repository_name)  # GitHub url https://github.com/apisdk/python-training
