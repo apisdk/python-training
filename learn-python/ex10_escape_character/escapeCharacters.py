34cc3f3c2a272a8f05cbe06d5eb22c8ee55c5b36
Taehoon.Yoon <nt10681@nhn.com>
2014-12-08 20:25:35 +0900

    Add example for escapeCharacter

---
+++ learn-python/ex10_escape_character/escapeCharacters.py
@@ -0,0 +1,12 @@
+message_for_print = "I \"understand\" Joe."
+
+print(message_for_print)
+
+print "I am 6'2\" tail."    # I am 6'2" tail.
+
+message_for_tab_characters1 = "index\tsubject\tdescription"
+message_for_tab_characters2 = "1\t\tmath\teasy"
+
+print(message_for_tab_characters1)
+print(message_for_tab_characters2)
+
