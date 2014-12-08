50cca612597a3dcc3f317da4f11f9c9620b7c8fa
Taehoon.Yoon <nt10681@nhn.com>
2014-12-08 18:51:14 +0900

    Add example for print strings to join

---
+++ learn-python/ex07_more_printing/printJoinString.py
@@ -0,0 +1,26 @@
+print "Mary had a little lamb." \
+      "appendString"
+
+print "a" * 5   # aaaaa
+
+char1   = "C"
+char2   = "h"
+char3   = "e"
+char4   = "e"
+char5   = "s"
+char6   = "e"
+char7   = "B"
+char8   = "u"
+char9   = "r"
+char10  = "g"
+char11  = "e"
+char12  = "r"
+
+# Cheese Burger
+print char1 + char2 + char3 + char4 + char5 + char6,
+print char7 + char8 + char9 + char10 + char11 + char12
+
+
+# CheeseBurger
+print char1 + char2 + char3 + char4 + char5 + char6 + \
+       char7 + char8 + char9 + char10 + char11 + char12
