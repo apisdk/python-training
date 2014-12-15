1352d9fe52ac687ad0c5c7f69618ffe7a41a2d93
Typoon <apisdk@gmail.com>
2014-12-15 16:22:51 +0900

    Add example for function of using boolean

---
+++ learn-python/ex27_memorizing_logic/booleanFunction.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+
+class Taste(Enum):
+    unknown = 0
+    sweet = 1
+    bitter = 2
+    sour = 3
+    salty = 4
+    hot = 5
+
+
+class Food(Enum):
+    apple = 0
+    orange = 1
+    snack = 2
+    cake = 3
+
+
+def isSweet(food):
+    if food == Food.apple:
+        return True
+    return False
+
+
+result = isSweet(Food.apple)
+print("%s isSweet=%s" % (Food.apple, result))
+
+result = isSweet(Food.orange)
+print("%s isSweet=%s" % (Food.orange, result))
+
