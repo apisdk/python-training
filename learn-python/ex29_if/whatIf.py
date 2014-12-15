7ab2a2892d75c5e796a2584e48a3bf795062bd95
Typoon <apisdk@gmail.com>
2014-12-15 17:23:04 +0900

    Add example for if comparison

---
+++ learn-python/ex29_if/whatIf.py
@@ -0,0 +1,14 @@
+people = 20
+
+cats = 10
+dogs = 15
+
+if (cats > people):
+    print("cats is greater than people.")
+else:
+    print("cats is not greater than people.")
+
+if (cats == dogs):
+    print("cats is equal to dogs")
+else:
+    print("cats is not equal to dogs")
