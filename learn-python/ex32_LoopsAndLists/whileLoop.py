9bea5317b5dce322109cba16666caa2f7e629141
Typoon <apisdk@gmail.com>
2014-12-17 20:45:12 +0900

    Add Example for Using while and operators

---
+++ learn-python/ex32_LoopsAndLists/whileLoop.py
@@ -0,0 +1,9 @@
+numbers = []
+
+i = 0
+while i < 10:
+    numbers.append(i)
+    i = i + 1
+
+for num in numbers:
+    print("number is %d" % num)
---
+++ learn-python/ex37_symbol/operators.py
@@ -0,0 +1,23 @@
+assert (1 + 1 == 2)
+
+assert (1 - 1 == 0)
+
+assert (2 * 2 == 4)
+
+assert (2 ** 4 == 16)
+
+assert (2.5 / 2 == 1.25)
+
+assert (2.125 / 2 == 1.0625)
+
+# floor division
+assert (2.125 // 2 == 1)
+
+item = {"name" : "apple", "count" : "1"}
+
+print(item.get("name"))
+
+def test():
+    return "test"
+
+print(test())
